432601cdca8035d8476e90049dc50bfd03fc53ec
phongnewimage <138086815+phongnewimage@users.noreply.github.com>
2023-06-29 14:10:46 +0700

    Update BI-Analysis.py

--- BI-Analysis.py
+++ BI-Analysis.py
@@ -4,11 +4,6 @@ import pyodbc
 import sqlalchemy
 from sqlalchemy import create_engine
 import urllib
-import matplotlib.pyplot as plt
-from pyecharts import options as opts
-from pyecharts.charts import Bar,Tab,Pie
-from pyecharts.commons.utils import JsCode
-from pyecharts.globals import ThemeType
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
