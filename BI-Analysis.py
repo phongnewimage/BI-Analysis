4015d35314449aed623f5662b43233d210d4a34c
phongnewimage <138086815+phongnewimage@users.noreply.github.com>
2023-06-29 14:10:05 +0700

    Update BI-Analysis.py

--- BI-Analysis.py
+++ BI-Analysis.py
@@ -1,3 +1,15 @@
 import streamlit as st
 import pandas as pd
+import pyodbc
+import sqlalchemy
+from sqlalchemy import create_engine
+import urllib
+import matplotlib.pyplot as plt
+from pyecharts import options as opts
+from pyecharts.charts import Bar,Tab,Pie
+from pyecharts.commons.utils import JsCode
+from pyecharts.globals import ThemeType
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 st.write('Hello')
